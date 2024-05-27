bff9c3dadedc49d0e637db53a00dc08f5e7323f4
miki-g <1222lassi@gmail.com>
2024-05-27 13:45:17 +0900

    test

--- src/main/java/com/example/demo/form/FormController.java
+++ src/main/java/com/example/demo/form/FormController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FormController {
+	@RequestMapping("/form")
 	@RequestMapping("/")
 	public String keijiban(Form form) {
 		
