632ed76a76df192c9b532787f6722774b43ef690
miki-g <1222lassi@gmail.com>
2024-05-27 13:35:31 +0900

    0527 formControllerの更新

--- src/main/java/com/example/demo/form/FormController.java
+++ src/main/java/com/example/demo/form/FormController.java
@@ -5,14 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FormController {
-
-	@RequestMapping("/form")
-	public String form(Form form) {
-		
-		return "form/input";
-	}
-
-	@RequestMapping("/keijiban")
+	@RequestMapping("/")
 	public String keijiban(Form form) {
 		
 		return "form/keijiban";
