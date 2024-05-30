e904024a9bcff4c3cd9a3d731ac803868c02e085
miki-g <1222lassi@gmail.com>
2024-05-30 14:06:20 +0900

    0530-2miki Commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE sample(
 	id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(100) NOT NULL,
-	comment VARCHAR(100) NOT NULL,
+	name VARCHAR(100) ,
+	comment VARCHAR(100) ,
 	datetime DATETIME,
 	PRIMARY KEY(id)
 );
